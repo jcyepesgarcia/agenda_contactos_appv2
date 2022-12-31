c4d5b740c8f1429d233af4d44202e14dfff9849f
jcyepesgarcia <jcyepesgarcia@gmail.com>
2022-12-31 08:53:51 -0500

    Inicio, vinculacion repositorio local/remoto

---
+++ src/main/java/com/aplicacionesjc/agenda_contactos_appv2/AgendaContactosAppv2Application.java
@@ -0,0 +1,13 @@
+package com.aplicacionesjc.agenda_contactos_appv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaContactosAppv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaContactosAppv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/aplicacionesjc/agenda_contactos_appv2/AgendaContactosAppv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplicacionesjc.agenda_contactos_appv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaContactosAppv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
