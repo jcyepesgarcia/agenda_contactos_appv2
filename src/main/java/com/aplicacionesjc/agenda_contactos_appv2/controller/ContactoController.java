8bb0ba9ed02029185d330049484169c4f09e7c51
jcyepesgarcia <jcyepesgarcia@gmail.com>
2023-01-03 11:12:37 -0500

    Validaciones agregadas

--- src/main/java/com/aplicacionesjc/agenda_contactos_appv2/controller/ContactoController.java
+++ src/main/java/com/aplicacionesjc/agenda_contactos_appv2/controller/ContactoController.java
@@ -5,6 +5,8 @@ import com.aplicacionesjc.agenda_contactos_appv2.repository.ContactoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -26,15 +28,19 @@ public class ContactoController {
 
     @GetMapping("/nuevo")
     public String mostrarFormularioDeRegistro(Model model){
-        model.addAttribute("nuevoContacto", new Contacto());
+        model.addAttribute("contacto", new Contacto());
         return "nuevo";
     }
 
     @PostMapping("/nuevo")
-    public String guardarContacto(Contacto contacto, RedirectAttributes redirect){
+    public String guardarContacto(@Validated Contacto contacto, BindingResult br,RedirectAttributes ra, Model model){
+        if(br.hasErrors()){
+            model.addAttribute("contacto", contacto);
+            return "nuevo";
+        }
         contacto.setFechaRegistro(LocalDateTime.now());
         contactoRepository.save(contacto);
-        redirect.addFlashAttribute("msgSuccess", "El contacto ha sido registrado con éxito.");
+        ra.addFlashAttribute("msgSuccess", "El contacto ha sido registrado con éxito.");
         return "redirect:/";
     }
 }
--- src/main/java/com/aplicacionesjc/agenda_contactos_appv2/model/Contacto.java
+++ src/main/java/com/aplicacionesjc/agenda_contactos_appv2/model/Contacto.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -16,10 +17,23 @@ public class Contacto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id_contacto")
     private Integer id;
+
+    @NotBlank(message = "Ingrese el nombre del contacto")
     private String nombre;
+
+    @NotBlank(message = "Ingrese el número del contacto")
+    @Size(max = 13)
     private String celular;
+
+    @Email(message = "Debe ingresar una dirección de correo electrónico con formato correcto")
     private String email;
+
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @Past(message = "Debe ingresar una fecha pasada")
+    @Column(name="fecha_nacimiento")
     private LocalDate fechaNacimiento;
+
+    @Column(name="fecha_registro")
     private LocalDateTime fechaRegistro;
+
 }
